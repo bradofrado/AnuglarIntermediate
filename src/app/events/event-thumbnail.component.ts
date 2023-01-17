18c5eb48226d65e1e52c2734a38daa939256b2d6
Braydon Jones <bradofrado@gmail.com>
2023-01-16 17:54:14 -0700

    Added styles and more data

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,10 +4,18 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
   selector: 'event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
+  .green { color: #003300 !important; }
+  .bold { font-weight: bold; }
+  .thumbnail { min-height: 210px; }
   .pad-left { margin-left: 10px; }
   .well div { color: #bbb; }
   `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return isEarlyStart ? ['green', 'bold'] : [];
+  }
 }
